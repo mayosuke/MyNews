5898ba155df20c0f934bd64520f1aa8fc3437243
mayosuke <mayo.suke@gmail.com>
2012-09-11 03:01:17 +0900

    ダミーのテストコードを追加した。

---
+++ TestMyNewsTest/src/jp/mayosuke/android/mynews/MainActivityTest.java
@@ -0,0 +1,14 @@
+package jp.mayosuke.android.mynews;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void testDummy() {
+        assertNotNull(null);
+    }
+}
