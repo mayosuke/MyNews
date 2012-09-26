24442b91c7e5e2d05dd1330ca3cf5631fcf37989
mayosuke <mayo.suke@gmail.com>
2012-09-26 09:19:40 +0900

    コードをクラス単位に分割した。

---
+++ src/jp/mayosuke/android/mynews/GoogleNews.java
@@ -0,0 +1,63 @@
+package jp.mayosuke.android.mynews;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+class GoogleNews {
+    private static final String TAG_CHANNEL = "channel";
+    private static final String TAG_IMAGE = "image";
+    private static final String TAG_ITEM = "item";
+
+    private final Stack<String> mState = new Stack<String>();
+    private final Map<String, String> mInfo = new HashMap<String, String>();
+    private final Map<String, String> mImageInfo = new HashMap<String, String>();
+    private final List<Map<String, String>> mItems = new ArrayList<Map<String, String>>();
+
+    private String mTag;
+
+    List<Map<String, String>> getItems() {
+        return mItems;
+    }
+
+    void onXmlEvent(final int eventType, final String value) {
+        switch (eventType) {
+        case XmlPullParser.START_TAG:
+            mTag = value;
+            if (value.equalsIgnoreCase(TAG_CHANNEL) ||
+                    value.equalsIgnoreCase(TAG_IMAGE) ||
+                    value.equalsIgnoreCase(TAG_ITEM)) {
+                mState.push(value);
+            }
+            if (value.equalsIgnoreCase(TAG_ITEM)) {
+                mItems.add(new HashMap<String, String>());
+            }
+            break;
+        case XmlPullParser.TEXT:
+            if (mState.peek().equalsIgnoreCase(TAG_CHANNEL)) {
+                mInfo.put(mTag, value);
+                break;
+            }
+            if (mState.peek().equalsIgnoreCase(TAG_IMAGE)) {
+                mImageInfo.put(mTag, value);
+            }
+            if (mState.peek().equalsIgnoreCase(TAG_ITEM)) {
+                mItems.get(mItems.size() - 1).put(mTag, value);
+            }
+            break;
+        case XmlPullParser.END_TAG:
+            if (value.equalsIgnoreCase(TAG_CHANNEL) ||
+                    value.equalsIgnoreCase(TAG_IMAGE) ||
+                    value.equalsIgnoreCase(TAG_ITEM)) {
+                mState.pop();
+            }
+            break;
+        default:
+            break;
+        }
+    }
+}
---
+++ src/jp/mayosuke/android/mynews/NewsDetailFragment.java
@@ -0,0 +1,110 @@
+package jp.mayosuke.android.mynews;
+
+import android.app.ListFragment;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.Map;
+
+public class NewsDetailFragment extends ListFragment {
+    private static final String TAG = NewsDetailFragment.class.getSimpleName();
+
+    private Map<String, String> mItem;
+    private Document mJsoup;
+
+    public NewsDetailFragment() {}
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.i(TAG, "onActivityCreated(savedInstanceState=" + savedInstanceState + ")");
+        super.onActivityCreated(savedInstanceState);
+
+        final Bundle args = getArguments();
+        mItem = (Map<String, String>) args.getSerializable(Constants.TAG_NEWS_ITEM);
+        final String html = "<html><head><meta http-equiv=\"content-type\" content=\"text/html;charset=UTF-8\"></head><body>" +
+                    mItem.get("description") + "</body></html>";
+        mJsoup = Jsoup.parse(html);
+        final Integer[] layouts = {
+                Integer.valueOf(R.layout.content_text),
+                Integer.valueOf(R.layout.content),
+                Integer.valueOf(R.layout.content_text),
+                Integer.valueOf(R.layout.content_text),
+        };
+        final ListAdapter adapter = new ArrayAdapter<Integer>(getActivity(), 0, layouts) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                final View view = View.inflate(getContext(), getItem(position).intValue(), null);
+                switch (position) {
+                case 0: {
+                    final TextView content = (TextView) view.findViewById(R.id.content);
+                    content.setText(Html.fromHtml(mJsoup.toString()));
+                    break;
+                }
+                case 1: {
+                    final WebView content = (WebView) view.findViewById(R.id.content);
+                    content.setWebViewClient(new WebViewClient() {
+                        @Override
+                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                            Log.i(TAG, "shouldOverrideUrlLoading(url=" + url + ")");
+                            return super.shouldOverrideUrlLoading(view, url);
+                        }
+                        @Override
+                        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                            Log.i(TAG, "onPageStarted(url=" + url + ")");
+                        }
+                        @Override
+                        public void onPageFinished(WebView view, String url) {
+                            Log.i(TAG, "onPageFinished(url=" + url + ")");
+                        }
+                    });
+                    content.getSettings().setJavaScriptEnabled(true);
+                    content.getSettings().setPluginsEnabled(true);
+                    content.loadDataWithBaseURL(null, html, "text/html", "UTF-8", null);
+                    break;
+                }
+                case 2: {
+                    final TextView content = (TextView) view.findViewById(R.id.content);
+                    content.setText(mJsoup.toString());
+                    break;
+                }
+                case 3: {
+                    final TextView content = (TextView) view.findViewById(R.id.content);
+                    Elements aTags = mJsoup.getElementsByTag("a");
+                    content.setText(aTags.toString());
+                    break;
+                }
+                default:
+                    // DO NOTHING.
+                    break;
+                }
+                return view;
+            }
+        };
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView(savedInstanceState=" + savedInstanceState + ")");
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+//        final View view = inflater.inflate(R.layout.news_detail, null);
+//        final View view = inflater.inflate(R.layout.activity_main, null);
+        view.setBackgroundColor(Color.WHITE);
+        return view;
+    }
+}
